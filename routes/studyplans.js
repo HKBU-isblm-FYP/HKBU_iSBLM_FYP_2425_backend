0e46d9020bba27f6a181e8fa2a55feb23a4d8d02
chan <s061402@gmail.com>
2024-11-17 15:02:49 +0800

    Fix Study Plan display

--- routes/studyplans.js
+++ routes/studyplans.js
@@ -20,20 +20,23 @@ const { connectToDB, ObjectId } = require('../utils/db');
 //     res.json(studyPlan);
 // });
 
+/**
+ * @route GET /studyPlans/:id
+ * @description Get study plan for a specific student -> Reformat the Progress into a study Plan array for feeding..
+ * @access Public
+ */
 router.get('/:id', async function (req, res, next) {
     const studentId = req.params.id;
-    let studyPlan = [];
+    let ProgressStudyPlan = [];
     const db = await connectToDB();
 
     try {
         // studyPlan = await db.collection('studyPlans').findOne({ sid: new ObjectId(studentId) });
-        studyPlan = await db.collection('projects').findOne({ 'member.0.id': new ObjectId(studentId) });
+        ProgressStudyPlan = await db.collection('projects').findOne({ 'member.0.id': new ObjectId(studentId) });
 
-        if (!studyPlan) {
+        if (!ProgressStudyPlan) {
             return res.status(404).json({ error: 'Study plan not found for the given student ID' });
         }
-
-        console.log(studyPlan);
 
         // Initialize an empty object to hold the reformatted study plan
         let reformattedStudyPlan = {
@@ -41,7 +44,7 @@ router.get('/:id', async function (req, res, next) {
         };
 
         // Iterate through the study plan array
-        studyPlan.progress.forEach(item => {
+        ProgressStudyPlan.progress.forEach(item => {
             // Create the year object if it doesn't exist
             if (!reformattedStudyPlan.studyPlan[`year${item.year}`]) {
                 reformattedStudyPlan.studyPlan[`year${item.year}`] = {
@@ -53,7 +56,7 @@ router.get('/:id', async function (req, res, next) {
 
             // Push the reformatted item to the appropriate semester array
             reformattedStudyPlan.studyPlan[`year${item.year}`][`semester${item.semester}`].push({
-                code: item.id,
+                code: item.courseCode,
                 name: item.title,
                 units: item.units
             });
@@ -63,16 +66,13 @@ router.get('/:id', async function (req, res, next) {
         });
 
         // Replace the studyPlan variable with the reformatted data
-        studyPlan = reformattedStudyPlan;
-
+        ProgressStudyPlan = reformattedStudyPlan;
         console.log("Reformatted", reformattedStudyPlan);
-
     } catch (err) {
         console.log(err);
         return res.status(500).json({ error: err.toString() });
     }
-
-    res.json(studyPlan);
+    res.json(ProgressStudyPlan);
 });
 
 
