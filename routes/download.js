b23709ad8670a4af83ab28f8e6ee0a7db2e69dcd
troyfung <fungtroy63@gmail.com>
2025-03-20 15:30:52 +0800

    Add .deployment and .vscode settings.json files, and update download.js

--- routes/download.js
+++ routes/download.js
@@ -18,5 +18,20 @@ router.get('/:blobName', async function (req, res) {
     res.status(400).json({ message: err.message });
   }
 });
+router.get('/:blobNames', async function (req, res) {
+  try {
+    // Get the blob name
+    console.log(1);
+    const blobName = req.params.blobName;
+    console.log(1);
+    // Generate a SAS token
+    const sasToken = generateBlobSasUri(blobName);
+    console.log(1);
+    // Return the SAS token
+    return res.json(sasToken);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = router;
