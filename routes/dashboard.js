170a05a8d8c82ce0284434173166ea7671b8ada1
chan <s061402@gmail.com>
2025-03-28 22:19:48 +0800

    Add student dashboard route and update admin dashboard logging

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 const { connectToDB, ObjectId } = require('../utils/db');
 
-// Route to get all lessons with pagination
+// Route to get information to fill in Admin Dashboard
 router.get('/', async (req, res) => {
-  console.log("Get Dashboard");
+  console.log("Get Admin Dashboard");
   const db = await connectToDB();
 
   try {
@@ -30,6 +30,36 @@ router.get('/', async (req, res) => {
     console.log(countStudentsByMajor);
 
     res.json({ courses: countCourses, projects: countProjects, users: countUsers, supervisors: countSupervisors, countStudentsByMajor: countStudentsByMajor });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  } finally {
+    // await db.client.close();
+  }
+});
+
+
+// Route to get information to fill student dashboard
+router.get('/student/:sid', async (req, res) => {
+  console.log("Get Student Dashboard");
+  const db = await connectToDB();
+
+  try {
+    const search = req.query.search || '';
+    const studentId = req.params.sid;
+    console.log("Student ID: ", studentId);
+    // const studentId = new ObjectId(req.params.sid);
+
+    const student = await db.collection('users').findOne({ _id: new ObjectId(studentId) });
+    const supervisor = await db.collection('users').findOne({ _id: new ObjectId(student.supervisor) });
+
+    delete student.password;
+    delete supervisor.password;
+
+    const countCourses = await db.collection('lessons').countDocuments();
+
+    res.json({ student: student, supervisor: supervisor, totalCourses: 89 });
 
   } catch (err) {
     console.log(err);
